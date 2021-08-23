bc2c52d7e6671b24f7372fd71907069d9e025977
kangzhaorui <1186785308@qq.com>
2021-08-23 17:57:30 +0800

    23

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-23 17:57:10
+ * @LastEditTime: 2021-08-23 17:57:17
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /localProject/weiqianduan/index.js
+ */
+console.log('-99999')
